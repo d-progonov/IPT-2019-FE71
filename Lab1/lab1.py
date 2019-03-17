7cd2e03f848483d31ea3c6dc2c802adbde40f946
Andriy Smidonov <yukihirosm@gmail.com>
2019-03-18 01:29:12 +0200

    Error fixes

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -1,6 +1,6 @@
 import math
+
 def function(x, z):
-	""" Counts ((x + 2x +3)/(z-2)) + arctg(z) """
     if z == 2:
         print("Введённое значение Z не принадлежит к ОДЗ.")
         print("Z должно быть не равно 2.")
@@ -9,20 +9,20 @@ def function(x, z):
         return ((x + 2*x + 3) / z - 2) + math.atan(z)
 
 while True:
-	input_X = input("Введите значение X --> ")
-	if not input_X.isnumeric():
-		print("Скорее всего вы ввели не число. Попробуйте снова: ")
-	else:
+	try:
+		input_X = input("Введите значение X --> ")
 		fX = float(input_X)
 		break
+	except ValueError:
+		print("Скорее всего вы ввели не число. Попробуйте снова: ")
 
 while True:
-	input_Z = input("Введите значение Z --> ")
-	if not input_Z.isnumeric():
-		print("Вы ввели не число. Попробуйте снова: ")
-	else:
+	try:
+		input_Z = input("Введите значение z --> ")
 		fZ = float(input_Z)
 		break
+	except ValueError:
+		print("Скорее всего вы ввели не число. Попробуйте снова: ")
 
 res = function(fX, fZ)
 if not res == "null":
