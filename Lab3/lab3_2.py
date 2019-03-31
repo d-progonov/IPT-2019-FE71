1bc35f8bb7385814e26ed853d4143f1f4fb64a6b
Andriy Smidonov <yukihirosm@gmail.com>
2019-04-01 01:18:05 +0300

    initial_lab_3

---
+++ Lab3/lab3_2.py
@@ -0,0 +1,16 @@
+'''Task: 16.Знайти суму ряду з точністю =10^-4, загальний член якого e^(n) * 100^(n*n)'''
+
+import math
+def getVal(n):
+    return math.exp(n)*(100**-(n*n))
+accuracy = 0.0001
+summ = 0.0
+n = 1
+while True:
+    if(getVal(n) >= accuracy):
+        summ += getVal(n)
+        print("{0} элемент последовательности: {1}".format(n, getVal(n)))
+        n += 1
+    else:
+        break
+print("Сумма последовательности с точностью 0.0001: {0}".format(summ))
