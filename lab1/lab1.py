abe4a7dfffbc967ab79edf6cb6343e32f7652b51
Khristuyk <48164821+Khristuyk@users.noreply.github.com>
2019-06-01 18:52:51 +0300

    Update lab1.py

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,1 +1,10 @@
 
+import math 
+z = float(input("ENTER Z: "))
+x = float(input("ENTER X: "))
+if ((z-(x**3)/3)==0) or (z*z-math.fabs(x**2/(z-(x**3)/3))==0):
+    print("ERROR!")
+else:
+    y = (z+(x/(z*z-math.fabs(x**2/(z-(x**3)/3)))))
+    print(y)
+
